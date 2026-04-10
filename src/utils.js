382d0f5610f0480271980f4fd9b035bef30076cd
winye1011 <wangxinyue897@gmail.com>
2026-04-10 17:52:47 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export const calculateSimilarity = (userLevels, targetCode, dimOrder) => {
+export function calculateSimilarity(userLevels, targetCode, dimOrder) {
   let score = 0;
   const targetArray = targetCode.split('');
   const levelMap = { 'H': 3, 'M': 2, 'L': 1 };
@@ -9,9 +9,9 @@ export const calculateSimilarity = (userLevels, targetCode, dimOrder) => {
     score += (2 - dist);
   });
   return Math.max(0, Math.round((score / (dimOrder.length * 2)) * 100));
-};
+}
 
-export const countAnswers = (answers, questions) => {
+export function countAnswers(answers, questions) {
   const counts = {};
   answers.forEach((ans, index) => {
     const q = questions[index];
@@ -21,16 +21,15 @@ export const countAnswers = (answers, questions) => {
     }
   });
   return counts;
-};
+}
 
-export const getLevelsFromCounts = (counts, config) => {
+export function getLevelsFromCounts(counts, config) {
   const levels = {};
-  const dims = config.dimensions || ['C', 'F', 'A', 'L'];
-  dims.forEach(dim => {
+  config.dimensions.forEach(dim => {
     const score = counts[dim] || 0;
     if (score >= 6) levels[dim] = 'H';
     else if (score <= 3) levels[dim] = 'L';
     else levels[dim] = 'M';
   });
   return levels;
-};
+}
