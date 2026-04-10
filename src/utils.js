384101018c2dcca775bcb6b053187efd2acc5af1
winye1011 <wangxinyue897@gmail.com>
2026-04-10 17:20:37 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,36 @@
-/**
- * Fisher-Yates 洗牌算法
- */
-export function shuffle(arr) {
-  const a = [...arr]
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[a[i], a[j]] = [a[j], a[i]]
-  }
-  return a
+export function calculateSimilarity(userLevels, targetCode, dimOrder) {
+  let score = 0
+  const targetArray = targetCode.split('')
+  const levelMap = { 'H': 3, 'M': 2, 'L': 1 }
+  
+  dimOrder.forEach((dim, index) => {
+    const userLevel = userLevels[dim] || 'M'
+    const targetLevel = targetArray[index] || 'M'
+    const dist = Math.abs(levelMap[userLevel] - levelMap[targetLevel])
+    score += (2 - dist)
+  })
+  return Math.max(0, Math.round((score / (dimOrder.length * 2)) * 100))
 }
 
-/**
- * 在数组的随机位置插入元素
- */
-export function insertAtRandom(arr, item) {
-  const a = [...arr]
-  const idx = Math.floor(Math.random() * (a.length + 1))
-  a.splice(idx, 0, item)
-  return a
+export function countAnswers(answers, questions) {
+  const counts = {}
+  answers.forEach((ans, index) => {
+    const q = questions[index]
+    if (q && q.options && q.options[ans]) {
+      const type = q.options[ans].type
+      counts[type] = (counts[type] || 0) + 1
+    }
+  })
+  return counts
 }
 
-/**
- * 在指定元素后面插入新元素
- */
-export function insertAfter(arr, afterId, item) {
-  const a = [...arr]
-  const idx = a.findIndex((q) => q.id === afterId)
-  if (idx >= 0) a.splice(idx + 1, 0, item)
-  return a
+export function getLevelsFromCounts(counts, config) {
+  const levels = {}
+  config.dimensions.forEach(dim => {
+    const score = counts[dim] || 0
+    if (score >= 6) levels[dim] = 'H'
+    else if (score <= 3) levels[dim] = 'L'
+    else levels[dim] = 'M'
+  })
+  return levels
 }
